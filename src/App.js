5ec2fe6366c754eaad5050d505ef5c18409143f9
Ben Hernandez <benjammin234@gmail.com>
2020-01-24 14:13:16 -0800

    Adds API fetch

--- src/App.js
+++ src/App.js
@@ -26,6 +26,42 @@ export default class App extends React.Component {
       showAddForm: false
     };
   }
+
+  componentDidMount() {
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer $2a$10$6.wiFdDtxlX4ATITf9J69uK8VbFnSt/YBHa1qtVJmvIfnHGm8RHry",
+        "Content-type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('You are not a Jedi, stop trying to use the force.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+        
+        
+        
+      
+  }
+
   render() {
     const page = this.state.showAddForm
       ? <AddBookmark />
